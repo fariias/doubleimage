39b50b07fd26c6585a236e420d0ad2a9f3b63508
Josenildo Farias <juniorfarias10@icloud.com>
2015-10-27 14:01:49 -0200

    adicionado: metodo de salvar imagem

--- main.c
+++ main.c
@@ -6,30 +6,30 @@ typedef struct
 } pixel;
 
 
-void ler_arquivo(char nome_arquivo[50], pixel imagem[512][512])
+void ler_arquivo(char nome_arquivo[50], pixel imagem[512][512], int *colunas, int *linhas, int *qualidade)
 {
 
 	FILE *Arquivo;
 	//definindo algumas variaveis locais
 	
 	char tipo[50];
-	int colunas,linhas,qualidade,i,j;
+	int i,j;
 	
 	
 	//abrindo o Arquivo 
 	Arquivo = fopen(nome_arquivo, "r");
 
 	fscanf(Arquivo,"%s", tipo);
-	fscanf(Arquivo,"%d", &colunas);
-	fscanf(Arquivo,"%d", &linhas);
-	fscanf(Arquivo,"%d", &qualidade);
+	fscanf(Arquivo,"%d", colunas);
+	fscanf(Arquivo,"%d", linhas);
+	fscanf(Arquivo,"%d", qualidade);
 
-	printf("%d %d %d\n", colunas,linhas,qualidade);
+	printf("%d %d %d\n", *colunas,*linhas,*qualidade);
 
 	//Salvando os pixels na matriz
-	for (i = 0; i < linhas; ++i)
+	for (i = 0; i < *linhas; ++i)
 	{
-		for (j = 0; j < colunas; ++j)
+		for (j = 0; j < *colunas; ++j)
 		{
 			fscanf(Arquivo, "%d", &(imagem[i][j].r));
 			fscanf(Arquivo, "%d", &(imagem[i][j].g));
@@ -40,37 +40,41 @@ void ler_arquivo(char nome_arquivo[50], pixel imagem[512][512])
 	
 }
 
-void imprimir_arquivo(pixel imagem[512][512])
+void salvar_arquivo(char nome_arquivo[50],pixel imagem[512][512],int colunas,int linhas,int qualidade)
 {
 	int i,j;
-for (i = 0; i < 512; ++i)
+	FILE *Arquivo;
+	Arquivo = fopen("Alterado--1--", "w");
+
+	fprintf (Arquivo, "P3\n");
+    fprintf (Arquivo, "%d ", colunas);
+    fprintf (Arquivo, "%d\n", linhas);
+    fprintf (Arquivo, "%d\n", qualidade);
+	for (i = 0; i < 512; ++i)
 	{
 		for (j = 0; j < 512; ++j)
 		{
-			printf("%d ", imagem[i][j].r);
-			printf("%d ", imagem[i][j].g);
-			printf("%d ", imagem[i][j].b);
+			fprintf(Arquivo,"%d ", imagem[i][j].r);
+			fprintf(Arquivo,"%d ", imagem[i][j].g);
+			fprintf(Arquivo,"%d\n", imagem[i][j].b);
 			
 		}
 	}
 }
+
 int main()
 {
 	//Definindo Variaveis
 	pixel imagem[512][512];
 	char tipo[50];
-	int largura,altura,qualidade;
-	int *pLargura;
-	pLargura = &largura;
-	
-
+	int colunas,linhas,qualidade;
 	char nome_arquivo[50];
 
 	//obtendo o caminho do arquivo
     printf("Informe o Caminho do Arquivo: ");
 	scanf("%s", nome_arquivo); 
 
-	ler_arquivo(nome_arquivo, imagem);
-	imprimir_arquivo(imagem);
+	ler_arquivo(nome_arquivo, imagem, &colunas, &linhas,&qualidade);
+	salvar_arquivo(nome_arquivo,imagem,colunas,linhas,qualidade);
 	return 0;
 }
